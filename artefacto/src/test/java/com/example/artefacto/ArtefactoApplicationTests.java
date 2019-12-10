e646ef03a65e59636a7bc44168ff253b4ca50a58
Alex <alexdeassis7@gmail.com>
2019-12-10 14:57:16 -0300

    commit one
    commit one

---
+++ artefacto/src/test/java/com/example/artefacto/ArtefactoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artefacto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtefactoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
